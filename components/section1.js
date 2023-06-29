b46f492a64d1ae20e06d4dda2facf236d3bd28ee
sd <2002sebastian@gmail.com>
2023-06-29 14:07:25 -0500

    style

--- components/section1.js
+++ components/section1.js
@@ -9,8 +9,8 @@ export default function section1( {cities} ) {
       </Head>
 
     <section className={"max-w-4xl mx-auto pt-16"}>
-      <h1 className={"text-3xl font-bold text-center"}>Trending</h1>
-
+      <h1 className={"text-2xl py-2 text-teal-600 text-center"}>City in a Day: Tailored 24-Hour Itineraries for the Urban Explorer</h1>
+      <h2 className={"text-md py-2 text-gray-800 text-center leading-8"}>Choose a city below to get started</h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
 
         {cities.map((city) => (
